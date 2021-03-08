546535d66082bfbf7e09f197ce5d86dd97069696
K95Monty <kathy95chapman@gmail.com>
2021-03-08 17:41:17 +0000

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,44 @@
+// Dropdown Menu
+	function openMenu() {
+		document.getElementById("nav-display").style.display = "block";
+		document.getElementById("menuHamburger").style.display = "none";
+		document.getElementById("menuX").style.display = "block";
+	}
+	function closeMenu() {
+		document.getElementById("nav-display").style.display = "none";
+		document.getElementById("menuHamburger").style.display = "block";
+		document.getElementById("menuX").style.display = "none";
+	}
+// end
+//Show Project Images
+	function showProject1() {
+		document.getElementById("projectImg1").style.display = "block";
+		document.getElementById("hideProjectHead1").style.display="inline-block";
+		document.getElementById("showProjectHead1").style.display="none";
+	}
+	function hideProject1() {
+		document.getElementById("projectImg1").style.display = "none";
+		document.getElementById("hideProjectHead1").style.display="none";
+		document.getElementById("showProjectHead1").style.display="inline-block";
+	}
+	function showProject2() {
+		document.getElementById("projectImg2").style.display = "block";
+		document.getElementById("hideProjectHead2").style.display="inline-block";
+		document.getElementById("showProjectHead2").style.display="none";
+	}
+	function hideProject2() {
+		document.getElementById("projectImg2").style.display = "none";
+		document.getElementById("hideProjectHead2").style.display="none";
+		document.getElementById("showProjectHead2").style.display="inline-block";
+	}
+	function showProject3() {
+		document.getElementById("projectImg3").style.display = "block";
+		document.getElementById("hideProjectHead3").style.display="inline-block";
+		document.getElementById("showProjectHead3").style.display="none";
+	}
+	function hideProject3() {
+		document.getElementById("projectImg3").style.display = "none";
+		document.getElementById("hideProjectHead3").style.display="none";
+		document.getElementById("showProjectHead3").style.display="inline-block";
+	}
+// end
