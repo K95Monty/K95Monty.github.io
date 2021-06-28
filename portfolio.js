5bc18a3f6831d81ef6dc587701a1afb2f0f545fd
K95Monty <48034846+K95Monty@users.noreply.github.com>
2021-06-28 22:01:35 +0100

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -13,32 +13,62 @@
 //Show Project Images
 	function showProject1() {
 		document.getElementById("projectImg1").style.display = "block";
+		document.getElementById("projectLink1").style.display = "block";
 		document.getElementById("hideProjectHead1").style.display="inline-block";
 		document.getElementById("showProjectHead1").style.display="none";
 	}
 	function hideProject1() {
 		document.getElementById("projectImg1").style.display = "none";
+		document.getElementById("projectLink1").style.display = "none";
 		document.getElementById("hideProjectHead1").style.display="none";
 		document.getElementById("showProjectHead1").style.display="inline-block";
 	}
 	function showProject2() {
 		document.getElementById("projectImg2").style.display = "block";
+		document.getElementById("projectLink2").style.display = "block";
 		document.getElementById("hideProjectHead2").style.display="inline-block";
 		document.getElementById("showProjectHead2").style.display="none";
 	}
 	function hideProject2() {
 		document.getElementById("projectImg2").style.display = "none";
+		document.getElementById("projectLink2").style.display = "none";
 		document.getElementById("hideProjectHead2").style.display="none";
 		document.getElementById("showProjectHead2").style.display="inline-block";
 	}
 	function showProject3() {
 		document.getElementById("projectImg3").style.display = "block";
+		document.getElementById("projectLink3").style.display = "block";
 		document.getElementById("hideProjectHead3").style.display="inline-block";
 		document.getElementById("showProjectHead3").style.display="none";
 	}
 	function hideProject3() {
 		document.getElementById("projectImg3").style.display = "none";
+		document.getElementById("projectLink3").style.display = "none";
 		document.getElementById("hideProjectHead3").style.display="none";
 		document.getElementById("showProjectHead3").style.display="inline-block";
+	}
+	function showProject4() {
+		document.getElementById("projectImg4").style.display = "block";
+		document.getElementById("projectLink4").style.display = "block";
+		document.getElementById("hideProjectHead4").style.display="inline-block";
+		document.getElementById("showProjectHead4").style.display="none";
+	}
+	function hideProject4() {
+		document.getElementById("projectImg4").style.display = "none";
+		document.getElementById("projectLink4").style.display = "none";
+		document.getElementById("hideProjectHead4").style.display="none";
+		document.getElementById("showProjectHead4").style.display="inline-block";
+	}
+		function showProject5() {
+		document.getElementById("projectImg5").style.display = "block";
+		document.getElementById("projectLink5").style.display = "block";
+		document.getElementById("hideProjectHead5").style.display="inline-block";
+		document.getElementById("showProjectHead5").style.display="none";
+	}
+	function hideProject5() {
+		document.getElementById("projectImg5").style.display = "none";
+		document.getElementById("projectLink5").style.display = "none";
+		document.getElementById("hideProjectHead5").style.display="none";
+		document.getElementById("showProjectHead5").style.display="inline-block";
 	}
 // end
